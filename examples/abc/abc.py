04e67f93b2d6f1ad2bba20bc93999c340e93d8ed
Mathias Soeken <mathias.soeken@epfl.ch>
2018-03-11 17:47:31 +0100

    Python example.

--- examples/abc/abc.py
+++ examples/abc/abc.py
@@ -1,9 +1,17 @@
+import sys
+sys.path.append("../../build/examples/abc")
+
 import abc2
 
 abc2.read_verilog(filename = "function.v")
 abc2.ps(wlc = True)
 abc2.convert(wlc_to_aig = True)
-abc2.ps(aig = True)
+nodes_before = abc2.ps(aig = True)["nodes"]
 abc2.syn3()
-abc2.ps(aig = True)
+nodes_after = abc2.ps(aig = True)["nodes"]
 abc2.write_aiger(filename = "/tmp/test.aig")
+
+if nodes_after < nodes_before:
+  print("Improvement :)")
+else:
+  print("No improvement :(")
