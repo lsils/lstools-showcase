c57f34a6290895923d8751d7214c996b615c92c3
Fereshte Mozafari <mozafari@tsf-460-wpa-0-237.epfl.ch>
2019-10-31 17:29:29 +0100

    complete grover algorithm

--- examples/grover/grover.cpp
+++ examples/grover/grover.cpp
@@ -16,7 +16,7 @@ int main(int argc , char** argv)
     (void) argc;
 	(void) argv;
 	using namespace tweedledum;
-	gg_network<mcmt_gate> network;
+	netlist<mcmt_gate> network;
 	network.add_qubit("q0");
 	network.add_qubit("q1");
     network.add_qubit("q2");
@@ -33,9 +33,12 @@ int main(int argc , char** argv)
     network.add_gate(gate::hadamard, "q3");
 
     /* -----oracle step----- */
-    network.add_gate(gate::x,"q1");
-    network.add_gate(gate::mcz, std::vector<std::string>({"q3", "q2" , "q1"}), std::vector<std::string>({"q0"}));
-    network.add_gate(gate::x,"q1");
+    std::vector<std::string> controls = {"q2", "q1" , "q0"};
+    std::vector<std::string> targets = {"q3"};
+
+    network.add_gate(gate::pauli_x,"q1");
+    network.add_gate(gate::mcz, controls, targets);
+    network.add_gate(gate::pauli_x,"q1");
 
     /* -----amplification step----- */
     network.add_gate(gate::hadamard, "q0");
@@ -43,17 +46,17 @@ int main(int argc , char** argv)
     network.add_gate(gate::hadamard, "q2");
     network.add_gate(gate::hadamard, "q3");
 
-    network.add_gate(gate::x, "q0");
-    network.add_gate(gate::x, "q1");
-    network.add_gate(gate::x, "q2");
-    network.add_gate(gate::x, "q3");
+    network.add_gate(gate::pauli_x, "q0");
+    network.add_gate(gate::pauli_x, "q1");
+    network.add_gate(gate::pauli_x, "q2");
+    network.add_gate(gate::pauli_x, "q3");
 
-    network.add_gate(gate::mcz, std::vector<std::string>({"q3", "q2" , "q1"}), std::vector<std::string>({"q0"}));
+    network.add_gate(gate::mcz, controls, targets);
 
-    network.add_gate(gate::x, "q0");
-    network.add_gate(gate::x, "q1");
-    network.add_gate(gate::x, "q2");
-    network.add_gate(gate::x, "q3");
+    network.add_gate(gate::pauli_x, "q0");
+    network.add_gate(gate::pauli_x, "q1");
+    network.add_gate(gate::pauli_x, "q2");
+    network.add_gate(gate::pauli_x, "q3");
 
     network.add_gate(gate::hadamard, "q0");
     network.add_gate(gate::hadamard, "q1");
@@ -61,13 +64,11 @@ int main(int argc , char** argv)
     network.add_gate(gate::hadamard, "q3");
 
     /* -----measurement step----- */
-    network.add_gate(gate::measurement, "q0" , "c0");
-    network.add_gate(gate::measurement, "q1" , "c1");
-    network.add_gate(gate::measurement, "q2" , "c2");
-    network.add_gate(gate::measurement, "q3" , "c3");
+    // network.add_gate(gate::measurement, "q0" , "c0");
+    // network.add_gate(gate::measurement, "q1" , "c1");
+    // network.add_gate(gate::measurement, "q2" , "c2");
+    // network.add_gate(gate::measurement, "q3" , "c3");
 
-
-	std::cout << "Hello world!\n";
 	write_unicode(network);
 
     return 1;
