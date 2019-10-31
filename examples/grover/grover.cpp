35923349600b1c17a1e6b7df2a3620cfa3f50ac5
Fereshte Mozafari <mozafari@tsf-460-wpa-0-237.epfl.ch>
2019-10-31 16:16:11 +0100

    update

--- examples/grover/grover.cpp
+++ examples/grover/grover.cpp
@@ -33,9 +33,9 @@ int main(int argc , char** argv)
     network.add_gate(gate::hadamard, "q3");
 
     /* -----oracle step----- */
-    network.add_gate(gate::x,"q1")
-    network.add_gate(gate::mcz, {3,2,1}, {0})
-    network.add_gate(gate::x,"q1")
+    network.add_gate(gate::x,"q1");
+    network.add_gate(gate::mcz, std::vector<std::string>({"q3", "q2" , "q1"}), std::vector<std::string>({"q0"}));
+    network.add_gate(gate::x,"q1");
 
     /* -----amplification step----- */
     network.add_gate(gate::hadamard, "q0");
@@ -48,7 +48,7 @@ int main(int argc , char** argv)
     network.add_gate(gate::x, "q2");
     network.add_gate(gate::x, "q3");
 
-    network.add_gate(gate::mcz, {3,2,1}, {0})
+    network.add_gate(gate::mcz, std::vector<std::string>({"q3", "q2" , "q1"}), std::vector<std::string>({"q0"}));
 
     network.add_gate(gate::x, "q0");
     network.add_gate(gate::x, "q1");
@@ -59,6 +59,12 @@ int main(int argc , char** argv)
     network.add_gate(gate::hadamard, "q1");
     network.add_gate(gate::hadamard, "q2");
     network.add_gate(gate::hadamard, "q3");
+
+    /* -----measurement step----- */
+    network.add_gate(gate::measurement, "q0" , "c0");
+    network.add_gate(gate::measurement, "q1" , "c1");
+    network.add_gate(gate::measurement, "q2" , "c2");
+    network.add_gate(gate::measurement, "q3" , "c3");
 
 
 	std::cout << "Hello world!\n";
